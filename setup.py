d0dc6aff127ff474b3959de3389c3bef67657453
ilovetux <me@ilovetux.com>
2015-06-12 11:25:22 -0400

    upped version number in an attempt to clean up pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='data.store',
-      version='0.6.7',
+      version='0.6.7.1',
       #description='a simple mongo-esque data_store.',
       author='iLoveTux',
       author_email='me@ilovetux.com',
